69bb281dbc26531c7cc23968712ff2a08195c035
Tuananh <tuananh48218@gmail.com>
2025-12-21 16:34:13 +0700

    chore: update dependencies in package.json
    - Added @nestjs/config, @nestjs/jwt, @prisma/adapter-pg, @prisma/client, @types/pg, class-transformer, class-validator, joi, and pg.
    - Updated typescript to version 5.9.3.
    - Added prisma as a dependency.

---
+++ apps/food-delivery/src/module/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, pass: string): Promise<unknown> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
---
+++ apps/food-delivery/src/module/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../../generated/prisma';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = `${process.env.DATABASE_URL}`;
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
