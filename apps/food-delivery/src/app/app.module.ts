7004690e0f711188c164e1b38ccff0d61cd16200
Tuananh <tuananh48218@gmail.com>
2025-12-21 16:34:13 +0700

    chore: update dependencies in package.json
    - Added @nestjs/config, @nestjs/jwt, @prisma/adapter-pg, @prisma/client, @types/pg, class-transformer, class-validator, joi, and pg.
    - Updated typescript to version 5.9.3.
    - Added prisma as a dependency.

--- apps/food-delivery/src/app/app.module.ts
+++ apps/food-delivery/src/app/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '../module/prisma/prisma.module';
+import { AuthModule } from '../module/auth/auth.module';
+import { UserModule } from '../module/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().port().default(3000),
+        DATABASE_URL: Joi.string().uri().required(),
+        JWT_SECRET_KEY: Joi.string().min(32).required(),
+        JWT_EXPIRES_IN: Joi.string().required(),
+      }),
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/food-delivery/src/module/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') as never, // ép kiểu string
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
