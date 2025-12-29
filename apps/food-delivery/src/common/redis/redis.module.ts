2e8bddb1f9c4a20088d5f297fc0159a70589d2d7
Tuananh <tuananh48218@gmail.com>
2025-12-30 00:33:20 +0700

    feat(redis): enhance Redis integration and implement order processing with BullMQ
    - Updated Docker configuration to increase CPU limits for services.
    - Added Redis service for inventory management and caching.
    - Implemented order processing using BullMQ for asynchronous job handling.
    - Introduced new Redis service methods for inventory decrement and batch operations.
    - Created initializers and processors for managing cache and order jobs.
    - Updated package dependencies to include @nestjs/bullmq and related packages.
    - Refactored order service to utilize Redis for inventory checks and caching responses.
    - Added new Prisma seed script for loading initial inventory data into Redis.

--- apps/food-delivery/src/common/redis/redis.module.ts
+++ apps/food-delivery/src/common/redis/redis.module.ts
@@ -1,9 +1,16 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { BullModule } from '@nestjs/bullmq';
+import { Cache_QUEUE, CacheProcessor } from './cache.processor';
 
 @Global()
 @Module({
-  providers: [RedisService],
-  exports: [RedisService]
+  imports: [
+    BullModule.registerQueue({
+      name: Cache_QUEUE,
+    }),
+  ],
+  providers: [RedisService, CacheProcessor],
+  exports: [RedisService, BullModule],
 })
 export class RedisModule {}
